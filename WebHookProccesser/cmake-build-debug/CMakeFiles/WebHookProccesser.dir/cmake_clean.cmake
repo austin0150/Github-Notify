272c1eec148fcffee4aa9a86081a6d654bc74bd9
Austin Johanningsmeier <ajohanni@umich.edu>
2018-11-08 20:40:35 -0500

    Created C Project
    Created C project to make the webhook proccessor

---
+++ WebHookProccesser/cmake-build-debug/CMakeFiles/WebHookProccesser.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WebHookProccesser.dir/main.c.obj"
+  "WebHookProccesser.pdb"
+  "WebHookProccesser.exe"
+  "WebHookProccesser.exe.manifest"
+  "libWebHookProccesser.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/WebHookProccesser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ WebHookProccesser/cmake-build-debug/CMakeFiles/WebHookProccesser.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for WebHookProccesser.
+# This may be replaced when dependencies are built.
